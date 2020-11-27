89779e8ca48903e8513668a8a63c84ea76c938a4
MIHAELA-MARIA POP <mihaela.maria.pop@stud.ubbcluj.ro>
2020-11-27 22:55:01 +0200

    Add project files.

---
+++ Data/Pop_Mihaela_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pop_Mihaela_Lab8.Models;
+
+namespace Pop_Mihaela_Lab8.Data
+{
+    public class Pop_Mihaela_Lab8Context : DbContext
+    {
+        public Pop_Mihaela_Lab8Context (DbContextOptions<Pop_Mihaela_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pop_Mihaela_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Pop_Mihaela_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Pop_Mihaela_Lab8.Models.Category> Category { get; set; }
+    }
+}
